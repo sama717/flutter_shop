d8e55c47a4df58200e613e5be8d9764a646ac440
sama717 <samafarag7@gmail.com>
2024-05-17 04:38:53 +0300

    Minor adjustments

--- lib/Widgets/home_app_bar.dart
+++ lib/Widgets/home_app_bar.dart
@@ -1,52 +1,55 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter/src/material/badge.dart';
-//import 'package:badges/badges.dart';
-class HomeAppBar extends StatelessWidget{
+import 'package:badges/badges.dart' as badges;
+
+class HomeAppBar extends StatelessWidget {
   const HomeAppBar({super.key});
 
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
       padding: const EdgeInsets.all(25),
-      child:   Row(children: [
-        const Icon(
-          Icons.sort,
-          size: 30,
-          color: Color(0xFF4C53A5),
-        ),
-        const Padding(padding: EdgeInsets.only(left: 20,),
-          child: Text("DP shop",
-            style:TextStyle(
-              fontSize: 23 ,
-              fontWeight: FontWeight.bold ,
-              color: Color(0xFF4C53A5),
+      child: Row(
+        children: [
+          const Icon(
+            Icons.sort,
+            size: 30,
+            color: Color(0xFF4C53A5),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              "DP shop",
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF4C53A5),
               ),
             ),
-        ),
-        const Spacer(),
-          Badge(
-          backgroundColor : Colors.red,
-          padding: const EdgeInsets.all(7),
-          //   badgeContent : const Text(
-          //   "3" ,
-          //  style: TextStyle(
-          //   color: Colors.white,
-          // ),
-          // ),
-
-          child: InkWell(
-            onTap: () {},
-            child: const Icon(
-              Icons.shopping_bag_outlined,
-              size: 32,
-              color: Color(0xFF4C53A5),
+          ),
+          const Spacer(),
+          badges.Badge(
+            badgeContent: const Text(
+              "3",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+            badgeStyle: const badges.BadgeStyle(
+              badgeColor: Colors.red,
+              padding: EdgeInsets.all(7),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: const Icon(
+                Icons.shopping_bag_outlined,
+                size: 32,
+                color: Color(0xFF4C53A5),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
       ),
-
     );
   }
 }
--- lib/Widgets/items_widget.dart
+++ lib/Widgets/items_widget.dart
@@ -1,105 +1,111 @@
 import 'package:flutter/material.dart';
-class ItemsWidget extends StatelessWidget{
+
+class ItemsWidget extends StatelessWidget {
   const ItemsWidget({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return GridView.count(
-    childAspectRatio: 0.68,
-      physics: const NeverScrollableScrollPhysics(),
-      crossAxisCount: 2,
-      shrinkWrap: true,
-      children: [
-        for(int i=1 ; i<11 ; i++)
-        Container(
-          padding: const EdgeInsets.only(left: 15 , right: 15 , top: 10),
-          margin: const EdgeInsets.symmetric(vertical: 8 , horizontal: 10),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: GridView.count(
+        childAspectRatio: 0.7, // Adjusted aspect ratio
+        crossAxisCount: 2,
+        shrinkWrap: true,
+        physics: const NeverScrollableScrollPhysics(), // Disable GridView's own scrolling
+        children: [
+          for (int i = 1; i < 11; i++)
+            Container(
+              padding: const EdgeInsets.all(8), // Reduced padding
+              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    padding: const EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color: const Color(0xFF4C53A5),
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: const Text(
-                      "-50%",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 14,
-                        color: Colors.white,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(5),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFF4C53A5),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: const Text(
+                          "-50%",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      const Icon(
+                        Icons.favorite_border,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                  InkWell(
+                    onTap: () {},
+                    child: Container(
+                      margin: const EdgeInsets.all(5), // Reduced margin
+                      child: Image.asset(
+                        "images/$i.jpg",
+                        height: 100, // Reduced height
+                        width: double.infinity, // Set to fill available width
+                        fit: BoxFit.cover, // Ensures the image covers the box
                       ),
                     ),
                   ),
-                  const Icon(
-                    Icons.favorite_border ,
-                    color: Colors.red,
-                  )
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 4), // Reduced padding
+                    child: const Text(
+                      "Product title",
+                      style: TextStyle(
+                        fontSize: 16, // Reduced font size
+                        color: Color(0xFF4C53A5),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const Expanded(
+                    child: Text(
+                      "Description of my product. Description of my product. Description of my product.",
+                      style: TextStyle(
+                        fontSize: 14, // Reduced font size
+                        color: Color(0xFF4C53A5),
+                      ),
+                      maxLines: 3, // Limits to 3 lines
+                      overflow: TextOverflow.ellipsis, // Adds ellipsis for overflow
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(top: 4), // Reduced padding
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "\$55",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xFF4C53A5),
+                          ),
+                        ),
+                        Icon(
+                          Icons.shopping_cart_checkout,
+                          color: Color(0xFF4C53A5),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-              InkWell(
-                onTap: (){} ,
-              child: Container(
-                margin: const EdgeInsets.all(10),
-                child: Image.asset("images/$i.jpg" ,
-                  height: 200 ,
-                  width: 200,
-                ),
-              ),
-              ),
-              Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                alignment: Alignment.centerLeft,
-                child: const Text(
-                    "Product title",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Color(0xFF4C53A5),
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Container(
-                alignment: Alignment.centerLeft,
-                child:const Text(
-                  "description of my product "
-                      "desciption of my product"
-                      " description of my product",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color:Color(0xFF4C53A5),
-                  ),
-                ),
-              ),
-              const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("\$55",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFF4C53A5),
-                    ),
-                    ),
-                    Icon(
-                        Icons.shopping_cart_checkout ,
-                      color: Color(0xFF4C53A5),),
-                  ],
-                ),
-              )
-            ],
-          ),
-        )
-      ],
+            ),
+        ],
+      ),
     );
   }
 }
