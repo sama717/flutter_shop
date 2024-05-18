2dd7d0f015ff7125f57f17f2700c5bd680767eea
Your Name <you@example.com>
2024-05-18 20:27:11 +0300

    commit

--- lib/Pages/item_page.dart
+++ lib/Pages/item_page.dart
@@ -6,19 +6,16 @@ import 'package:clippy_flutter/clippy_flutter.dart';
 import 'package:flutter_shop/Widgets/itemBottomNavBar.dart';
 
 class ItemPage extends StatelessWidget {
-
-  final List<Color> Clrs=[
+  final List<Color> clrs=[
     Colors.red,
     Colors.green,
     Colors.blue,
     Colors.indigo,
     Colors.orange,
   ];
-
   ItemPage({super.key});
   @override
   Widget build(BuildContext context) {
-    var $i;
     return Scaffold(
       backgroundColor: const Color(0xFFEDECF2),
       body: ListView(
@@ -29,182 +26,141 @@ class ItemPage extends StatelessWidget {
             child: Image.asset("images/1.jpg", height: 300),
           ),
           Arc(
-            edge:Edge.TOP,
             arcType: ArcType.CONVEY,
+            edge:Edge.TOP,
             height: 30,
             child: Container(
               width: double.infinity,
               height: double.infinity,
               color: Colors.white,
               child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    children: [
-                      const Padding(
-                          padding: EdgeInsets.only(
-                            top: 48,
-                            bottom: 15,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(
+                        top: 48,
+                        bottom: 15,
+                      ),
+                      child: Row(
+                        children: [
+                          Text("Product Title",
+                            style: TextStyle(
+                              fontSize: 28,
+                              color: Color(0xFF4C53A5),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                          child: Row(
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 5,bottom: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          RatingBar.builder(
+                            initialRating:4,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            itemCount: 5,
+                            itemSize: 20,
+                            itemPadding: const EdgeInsets.symmetric(horizontal: 4),
+                            itemBuilder: (context, _) => const Icon(
+                              Icons.favorite,
+                              color: Color(0xFF4C53A5),
+                            ),
+                            onRatingUpdate: (index){},
+                          ),
+                          Row(
                             children: [
-                              Text("Product Title",
-                                style: TextStyle(
-                                  fontSize: 28,
+                              Container(
+                                padding: const EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        spreadRadius: 3,
+                                        blurRadius: 10,
+                                        offset: const Offset(0, 3),
+                                      ),
+                                    ]
+                                ),
+                                child: const Icon(
+                                  CupertinoIcons.minus,
+                                  size: 18,
                                   color: Color(0xFF4C53A5),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.symmetric(horizontal: 10),
+                                child: const Text("01", style: TextStyle(
+                                  fontSize: 18,
                                   fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-                      ),
-                      Padding(
-                          padding: const EdgeInsets.only(top: 5,bottom: 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              RatingBar.builder(
-                                initialRating:4,
-                                minRating: 1,
-                                direction: Axis.horizontal,
-                                itemCount: 5,
-                                itemSize: 20,
-                                itemPadding: const EdgeInsets.symmetric(horizontal: 4),
-                                itemBuilder: (context, _) => const Icon(
-                                  Icons.favorite,
                                   color: Color(0xFF4C53A5),
                                 ),
-                                onRatingUpdate: (index){},
-                              ),
-                              Row(
-                                children: [
-                                  Container(
-                                    padding: const EdgeInsets.all(5),
-                                    decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(20),
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: Colors.grey.withOpacity(0.5),
-                                          spreadRadius: 3,
-                                          blurRadius: 10,
-                                          offset: const Offset(0, 3),
-                                        ),
-                                      ]
-                                    ),
-                                    child: const Icon(
-                                        CupertinoIcons.minus,
-                                        size: 18,
-                                        color: Color(0xFF4C53A5),
-                                    ),
-                                  ),
-                                  Container(
-                                    margin: const EdgeInsets.symmetric(horizontal: 10),
-                                    child: const Text("01", style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                      color: Color(0xFF4C53A5),
-                                    ),
 
-                                    ),
-                                  ),
-                                  Container(
-                                    padding: const EdgeInsets.all(5),
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(20),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: Colors.grey.withOpacity(0.5),
-                                            spreadRadius: 3,
-                                            blurRadius: 10,
-                                            offset: const Offset(0, 3),
-                                          ),
-                                        ]
-                                    ),
-                                    child: const Icon(
-                                      CupertinoIcons.plus,
-                                      size: 18,
-                                      color: Color(0xFF4C53A5),
-                                    ),
-                                  ),
-                                ],
+                                ),
+                              ),
+                              Container(
+                                padding: const EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.grey.withOpacity(0.5),
+                                        spreadRadius: 3,
+                                        blurRadius: 10,
+                                        offset: const Offset(0, 3),
+                                      ),
+                                    ]
+                                ),
+                                child: const Icon(
+                                  CupertinoIcons.plus,
+                                  size: 18,
+                                  color: Color(0xFF4C53A5),
+                                ),
                               ),
                             ],
                           ),
+                        ],
                       ),
-                      const Padding(padding: EdgeInsets.symmetric(vertical: 12),
-                        child: Text(
-                          "this is more detailed description of product",
-                          textAlign: TextAlign.justify,
-                          style: TextStyle(
-                            fontSize: 17,
-                            color: Color(0xFF4C53A5),
-                          ),
+                    ),
+                    const Padding(padding: EdgeInsets.symmetric(vertical: 12),
+                      child: Text(
+                        "this is more detailed description of product",
+                        textAlign: TextAlign.justify,
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Color(0xFF4C53A5),
                         ),
                       ),
-                      Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 8),
-                          child: Row(
-                            children: [
-                              const Text("size:",
-                                style: TextStyle(
-                                        fontSize: 18,
-                                         color: Color(0xFF4C53A5),
-                                           fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              const SizedBox(width: 10,),
-                              Row(children: [
-                                for(int i = 0;i < 5; i++)
-                                  Container(
-                                    height: 30,
-                                    width: 30,
-                                    alignment: Alignment.center,
-                                    margin: const EdgeInsets.symmetric(horizontal: 5),
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius: BorderRadius.circular(30),
-                                        boxShadow: [
-                                          BoxShadow(
-                                            color: Colors.grey.withOpacity(0.5),
-                                            spreadRadius: 2,
-                                            blurRadius: 8,
-                                          ),
-                                        ]
-                                    ),
-                                    child: Text(
-                                       $i.toString(),
-                                      style: const TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                        color: Color(0xFF4C53A5),
-                                      ),),
-                                  ),
-                              ],)
-                            ],
-                          ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8),
-                        child: Row(
-                          children: [
-                            const Text("color:",
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Color(0xFF4C53A5),
-                                fontWeight: FontWeight.bold,
-                              ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: Row(
+                        children: [
+                          const Text("size:",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xFF4C53A5),
+                              fontWeight: FontWeight.bold,
                             ),
-                            const SizedBox(width: 10,),
-                            Row(children: [
-                              for(int i=0;i<5;i++)
+                          ),
+                          const SizedBox(width: 10,),
+                          Row(
+                            children: [
+                              for(int i = 5;i <10; i++)
                                 Container(
                                   height: 30,
                                   width: 30,
                                   alignment: Alignment.center,
                                   margin: const EdgeInsets.symmetric(horizontal: 5),
                                   decoration: BoxDecoration(
-                                      color: Clrs[i],
+                                      color: Colors.white,
                                       borderRadius: BorderRadius.circular(30),
                                       boxShadow: [
                                         BoxShadow(
@@ -212,19 +168,59 @@ class ItemPage extends StatelessWidget {
                                           spreadRadius: 2,
                                           blurRadius: 8,
                                         ),
-                                      ]
-                                  ),
-
+                                      ]),
+                                  child: Text(
+                                    i.toString(),
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Color(0xFF4C53A5),
+                                    ),),
                                 ),
                             ],)
-                          ],
-                        ),
+                        ],
                       ),
-                    ],
-                  ),
-              ),
-            ),
-          ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 8),
+                      child: Row(
+                        children: [
+                          const Text("Color:",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xFF4C53A5),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(width: 10,),
+                          Row(
+                            children: [
+                              for(int i = 0;i < 5; i++)
+                                Container(
+                                    height: 30,
+                                    width: 30,
+                                    alignment: Alignment.center,
+                                    margin: const EdgeInsets.symmetric(horizontal: 5),
+                                    decoration: BoxDecoration(
+                                        color: clrs[i],
+                                        borderRadius: BorderRadius.circular(30),
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: Colors.grey.withOpacity(0.5),
+                                            spreadRadius: 2,
+                                            blurRadius: 8,
+                                          ),
+                                        ])
+
+                                ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),),),
         ],
       ),
       bottomNavigationBar: const ItemBottomNavBar(),
